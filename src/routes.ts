c25b5c8768326a45375e2173b3c4123a05eae1c3
SivaramPg <sivarampg95@gmail.com>
2020-11-15 14:40:53 +0530

    update root route

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,8 @@
 import { Application } from 'express';
 
 export const routes = (app: Application) => {
+  // Health Check Route for the Load Balancer
   app.get('/', (req, res) => {
-    res.status(200).send('Hello Hello');
+    res.status(200).send('Server is up!');
   });
 };
