38ad4f2b572ec7d748ff4d840ad1fc85b73b4ed0
SivaramPg <sivarampg95@gmail.com>
2020-11-15 14:41:24 +0530

    add exception and rejection handlers

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,51 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
 import { routes } from './routes';
 import { middlewares } from './middlewares';
 
-export const app = express();
+process.on('uncaughtException', (error) => {
+  console.log('Something terrible happened: ', error);
+  process.exit(1); // exit application
+});
 
-routes(app);
+process.on('unhandledRejection', (error, promise) => {
+  console.log('We forgot to handle a promise rejection here: ', promise);
+  console.log('The error was: ', error);
+});
+
+export const app = express();
+/**
+ * ! Middleware applied to the application inside the middlewares.js file.
+ */
 middlewares(app);
+
+/**
+ * ! All Route definitions go inside routes.js file
+ */
+routes(app);
+
+// Catch all handler to set route not found.
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next({
+    status: 404,
+    message: 'Not Found',
+  });
+});
+
+interface ErrorWithStatus extends Error {
+  status?: number;
+}
+
+// Global error handler function
+app.use(
+  (err: ErrorWithStatus, req: Request, res: Response, next: NextFunction) => {
+    if (err.name === 'UnauthorizedError') {
+      res.status(401).send('invalid token...');
+    }
+    if (err.status === 404) {
+      res.status(err.status).send(err.message);
+    } else if (err.status === 500) {
+      res.status(err.status).send(err.message);
+    }
+  }
+);
